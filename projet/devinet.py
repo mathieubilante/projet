328cd7735928059292bf1ebd5f8c94d7cb6940f7
mathieubilante <m97782840@gmail.com>
2025-09-14 23:13:20 +0000

    devinete des nombre

---
+++ projet/devinet.py
@@ -0,0 +1,21 @@
+import random
+
+nombre= random.randint(1, 100)
+#intiallisation de nombre d'essai
+essai= 0
+print(" le jeu de devinettes des nombre aleatoirze")
+print("veiller etre pret pout entre votre proposition")
+#la saisir de la proposition
+devine =int(input("veiller entrer ta proposition : "))
+essai +=1
+    if devine< nombre :
+     print("la valeur est grande")
+  elif devine> nombre :
+     print("La valeur est plus petite")
+  else:
+        print(" bravo! vous avez trouver")
+        break
+   except valeur_erreur
+        print(" Merci d'entrer la valeur valide")
+        
+        
