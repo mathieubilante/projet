7813e8376924fd920bf9de8602232de2b004a272
mathieubilante <m97782840@gmail.com>
2025-09-14 23:18:37 +0000

    debut du semetre  pour apprendfe python

---
+++ projet/debut.py
@@ -0,0 +1,18 @@
+'''Apprendre a coder ,revision d du vendredi'''
+"""apprentissage de la python"""
+# définir une variable de type entier
+nombre = 5
+# Affichage de la variable
+print ("La valeur de n est :" , nombre)
+def mafonction(x):
+    return 2*x + 4*x**2 -7
+print("la fonction saisi est :", mafonction(0))
+age= int(input("Entrer votre age :"))
+if (age<=18):
+    print(("vous ete mineur "))
+elif age<=25:
+        print(("vous ete jeune"))
+else:
+    print("vous ete vous ete vieux/vielle ,courage a toi ")
+    
+    
